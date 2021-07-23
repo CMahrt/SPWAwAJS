e02f7d2b1aac32d9d2ff6de434e408f580d04a42
Christian Mahrt <cm@deedac.com>
2021-07-23 10:18:07 +0200

    commit_1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,6 +1,36 @@
 (function () {
 'use strict';
 
+angular
+  .module('LunchCheck',[])
+  .controller('Mod1Controller',Mod1Controller);
+
+Mod1Controller.$inject = ['$scope'];
+function Mod1Controller($scope){
+  $scope.lunchlist="";
+  $scope.checkResult="";
+
+  $scope.checkLunchList=function (){
+    $scope.checkResult="";
+    var list=$scope.lunchlist.toString().split(",");
+    var filterEmptyElements=function (item) {
+      return item.toString().trim().length>0;
+    };
+    var filteredList=list.filter(filterEmptyElements);
+    var lunchListLenght=filteredList.length;
+    if(lunchListLenght>0){
+      $scope.textcolor="green";
+      if(lunchListLenght<=3){
+        $scope.checkResult="Enjoy!";
+      }else{
+        $scope.checkResult="Too much!";
+      }
+    }else{
+        $scope.textcolor="red";
+        $scope.checkResult="Please enter data first"
+    }
+  }
+}
 
 
 })();
